49ad7917e2d7a36ede1ce23bf15dc479d4cafb4a
pthsboy <91539434+pthsboy@users.noreply.github.com>
2025-12-05 11:32:08 +0800

    Update add

--- main.c
+++ main.c
@@ -4,6 +4,9 @@ int main() {
     // 修正：\n 是換行符號，中間不能有空白 (原本寫成 \ n)
     printf("=== C Language Calculator ===\n");
     
+    int a=10,b=20;
+    int sum=a+b;
+    printf("add 10+ 20= %d\n",sum);
     // Operations placeholders 
     printf("System Check OK.\n"); 
     
