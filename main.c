6e45bdf9f4df1d6e1d9e43dd529e91cff69ce84e
KenHuang21163 <d1285092@o365.fcu.edu.tw>
2025-12-05 11:07:49 +0800

    feat: add subtraction operation

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>  
+#include <stdio.h>
 
 int main() {
-    // 修正：\n 是換行符號，中間不能有空白 (原本寫成 \ n)
     printf("=== C Language Calculator ===\n");
-    
-    // Operations placeholders 
-    printf("System Check OK.\n"); 
-    
+
+    int x = 50, y = 30;
+    int diff = x - y;
+    printf("Sub: %d - %d = %d\n", x, y, diff);
+
+    printf("System Check OK.\n");
     return 0;
 }
