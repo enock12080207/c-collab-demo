389a74897f6b634c4d7574c538c22a497a76dd24
enock12080207 <kaisermarvel1018@gmail.com>
2025-12-05 10:19:07 +0800

    chore: init project structure

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+
+int main() {
+    // 修正：\n 是換行符號，中間不能有空白 (原本寫成 \ n)
+    printf("=== C Language Calculator ===\n");
+    
+    // Operations placeholders 
+    printf("System Check OK.\n"); 
+    
+    return 0;
+}
